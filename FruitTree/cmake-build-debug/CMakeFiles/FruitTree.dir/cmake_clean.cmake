f4fd7a7e71ca09272b08b14b876e5c1c1f04cb64
OmarAshraf <53156581+OmarAshraf1@users.noreply.github.com>
2021-06-15 17:59:44 -0700

    Add files via upload

---
+++ FruitTree/cmake-build-debug/CMakeFiles/FruitTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FruitTree.dir/main.cpp.obj"
+  "FruitTree.exe"
+  "FruitTree.exe.manifest"
+  "FruitTree.pdb"
+  "libFruitTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FruitTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
